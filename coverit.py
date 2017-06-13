b444168abcce7d878270d1dd2a956af81164b2e2
nctamer <ncntamer@gmail.com>
2017-06-13 13:59:38 +0300

    banderasicin_multifilter

--- coverit.py
+++ coverit.py
@@ -3,12 +3,12 @@ import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 
-y, sr = librosa.load('celloA3.mp3', sr=None)
+y, sr = librosa.load('banderas.mp3', sr=None)
 
-cqt = librosa.core.cqt(y, sr=sr)
-#librosa.display.specshow(librosa.amplitude_to_db(cqt, ref=np.max),
-#                         sr=sr, x_axis='time', y_axis='cqt_note')
-librosa.display.specshow(np.abs(cqt),
-                         sr=sr, x_axis='time', y_axis='cqt_note')
+cqt = librosa.amplitude_to_db(librosa.core.cqt(y, sr=sr))
+librosa.display.specshow(cqt, sr=sr, x_axis='time', y_axis='cqt_note')
 plt.title('Constant-Q power spectrum')
 plt.tight_layout()
+
+onset = librosa.onset.onset_strength_multi(S=cqt, sr=sr, max_size=84)
+librosa.display.specshow(onset, sr=sr, x_axis='time', y_axis='cqt_note')
